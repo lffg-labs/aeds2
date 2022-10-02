74e8d98d6921d0c4ed70bbee301b53feeb9b4c91
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2022-10-02 14:25:34 -0300

    refac(string): minor refactors

--- common/string.c
+++ common/string.c
@@ -8,73 +8,78 @@
 
 #define DEBUG_CONTROL 1
 
-#define DEBUG(fmt, ...)                                       \
-    do {                                                      \
-        if (DEBUG_CONTROL) fprintf(stderr, fmt, __VA_ARGS__); \
+#define DEBUG(...)                        \
+    do {                                  \
+        if (DEBUG_CONTROL) {              \
+            fprintf(stdout, __VA_ARGS__); \
+        }                                 \
+    } while (0)
+
+#define ASSERT(cond, ...)                 \
+    do {                                  \
+        if (!(cond)) {                    \
+            fprintf(stdout, __VA_ARGS__); \
+            exit(1);                      \
+        }                                 \
     } while (0)
 
 // -----------------------------------------------------------------------------
-// Dynamically allocated string.
+// Dynamically allocated string
 
 // A null-terminated, growable "string" buffer.
 //
 // Yes, it could have been not null-terminated, but it would make its usage
 // harder.
 typedef struct String {
-    int cap;
-    int len;
     char *buf;
+    int len;
+    int cap;
 } String;
 
-void String_internal_alloc(String *self, int cap) {
+static void String_internal_alloc(String *self, int cap) {
+    ASSERT(cap != 0, "fatal: can't allocate zero-sized string\n");
     DEBUG("string: allocating %d\n", cap);
-    self->cap     = cap;
-    char *new_buf = (char *)malloc(sizeof(char) * cap);
-    if (new_buf == NULL || cap == 0) {
-        fprintf(stderr, "fatal: couldn't allocate string\n");
-        exit(1);
-    }
-    self->buf = new_buf;
+
+    char *new_buf = (char *)calloc(cap, sizeof(char));
+    ASSERT(new_buf != NULL, "fatal: couldn't allocate string\n");
+
+    new_buf[0] = 0;
+    self->buf  = new_buf;
+    self->cap  = cap;
 }
 
-void String_internal_grow_for(String *self, int needed_space) {
+static void String_internal_grow_for(String *self, int needed_space) {
     int curr_cap = self->cap;
     int len = self->len + 1;  // One should account for the null terminator.
-    DEBUG(
-        "string: attempting to grow more %d"
-        " (curr len + 1 [%d]) (current cap [%d])\n",
-        needed_space, len, curr_cap);
+    DEBUG("attempting to allocate +%d (real len %d) (cap %d) ", needed_space,
+          len, curr_cap);
 
     while (len + needed_space > curr_cap) {
-        curr_cap *= 2;
+        curr_cap *= 2;  // TODO: Make this O(1).
     }
     if (curr_cap == self->cap) {
+        DEBUG("<fit>\n");
         return;  // Already have the capacity.
     }
 
+    DEBUG("<will realloc>\n");
     char *old = self->buf;
     String_internal_alloc(self, curr_cap);  // Allocate a new buffer.
-    for (int i = 0; i < len; i++) {
-        self->buf[i] = old[i];
-    }
+    memcpy(self->buf, old, len);
     free(old);  // Free the old buffer.
 }
 
 void String_free(String s) {
+    ASSERT(s.buf != NULL, "fatal: double free\n");
     free(s.buf);
+    s.buf = NULL;
 }
 
 // Allocates a new string, provided the given capacity (which cannot be zero).
 String String_alloc(int cap) {
     String s;
-    s.len    = 0;
-    s.cap    = cap;
-    s.buf    = malloc(sizeof(char) * cap);
-    s.buf[0] = 0;
-    if (s.buf == NULL) {
-        fprintf(stderr, "fatal: couldn't allocate string\n");
-        exit(1);
-    }
+    String_internal_alloc(&s, cap);
+    s.len = 0;
     return s;
 }
 
@@ -87,27 +92,25 @@ void String_clear(String *self) {
 // Pushes a single character at the end of the string.
 void String_push(String *self, char c) {
     String_internal_grow_for(self, 1);
-    self->buf[self->len]     = c;
-    self->buf[self->len + 1] = 0;
+    self->buf[self->len++] = c;
+    self->buf[self->len]   = 0;
 }
 
-void String_append_char_arr(String *self, int len, char *str) {
+void String_append_char_arr(String *self, int len, const char *str) {
     // Notice that one is not passing `len + 1` since the current `String *self`
     // is already null terminated. This invariant is kept by the String
     // implementation itself.
     String_internal_grow_for(self, len);
 
     int new_len = self->len;
-    for (int i = 0; i < len; i++) {
-        self->buf[new_len + i] = str[i];
-    }
+    memcpy(&self->buf[new_len], str, len);
     new_len += len;
     self->buf[new_len] = 0;
     self->len          = new_len;
 }
 
 // Appends another String at the end of the current (self) string.
-void String_append(String *self, String *other) {
+void String_append(String *self, const String *other) {
     String_append_char_arr(self, other->len, other->buf);
 }
 
@@ -132,6 +135,8 @@ int read_line(FILE *f, int buf_cap, char *buf, String *dest) {
     String_clear(dest);
     int len;
     do {
+        // Since one utilizes fgets here, this implementation can not left
+        // "forgotten" characters which may apper after a new line.
         char *str = fgets(buf, buf_cap, f);
         if (str == NULL) {
             return 1;
@@ -149,21 +154,42 @@ int read_line(FILE *f, int buf_cap, char *buf, String *dest) {
 // -----------------------------------------------------------------------------
 // Main
 
+void print_debug_string(String *str) {
+    printf("[%s] (len %d) (cap %d)\n", str->buf, str->len, str->cap);
+}
+
 int main() {
     char buf[8];
-    String s = String_alloc(2);
+    String s1 = String_alloc(2);
 
     while (!feof(stdin)) {
-        if (read_line(stdin, sizeof(buf), buf, &s) == 1) {
+        if (read_line(stdin, sizeof(buf), buf, &s1) == 1) {
             if (feof(stdin)) {
                 break;  // EOF error condition.
             }
-            fprintf(stderr, "fatal: couldn't read string\n");
-            return 1;
+            ASSERT(1, "fatal: couldn't read string");
         }
 
-        printf("[%s] (len %d)\n", s.buf, s.len);
+        print_debug_string(&s1);
+        printf("\n");
     }
+
+    printf("---\n");
+
+    String s2 = String_alloc(1);
+    String_append_char_arr(&s2, 3, "abc");
+    print_debug_string(&s2);
+
+    String_push(&s2, 'd');
+    print_debug_string(&s2);
+
+    printf("\n");
+    String s3 = String_alloc(4);
+    String_append_char_arr(&s3, 3, "efg");
+    printf("\n");
+
+    String_append(&s2, &s3);
+    print_debug_string(&s2);
 
     return 0;
 }
